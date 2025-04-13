d546e2867f290397f719eb1057223ef01aefbc11
Artyom <Ya.zloy246@gmail.com>
2025-04-13 18:54:51 +0500

    Enemy movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,8 +1,23 @@
 using System;
-using Unity.VisualScripting;
+using UnityEditor.U2D.Aseprite;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Enemy: MonoBehaviour
 {
-    
+    public float enemySpeed;
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        if (Vector2.Distance(player.transform.position, transform.position) <= 7)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, enemySpeed * Time.deltaTime);
+        }
+    }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEditor.Searcher;
 using UnityEngine;
 
@@ -8,7 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public float deceleration = 10f;
     private Rigidbody2D rb;
     private Vector2 currentVelocity;
-    public Animator animator;
+    public int Health = 10;
 
     void Start()
     {
@@ -22,5 +23,18 @@ public class PlayerMovement : MonoBehaviour
 
         Vector2 targetVelocity = new Vector2(moveX, moveY) * moveSpeed;
         rb.linearVelocity = Vector2.SmoothDamp(rb.linearVelocity, targetVelocity, ref currentVelocity, acceleration * Time.deltaTime);
+
+        if (Health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Health -= 1;
+        }
     }
 }
