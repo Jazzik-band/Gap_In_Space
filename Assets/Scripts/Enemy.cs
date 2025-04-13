ee12e6923253afe2b6bfabc11766c9ac0dbcb5b9
Artyom <Ya.zloy246@gmail.com>
2025-04-13 20:10:03 +0500

    Enemy walk

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,23 +1,34 @@
-using System;
-using UnityEditor.U2D.Aseprite;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class Enemy: MonoBehaviour
 {
-    public float enemySpeed;
+    [SerializeField] private float radius = 2f;
+    [SerializeField] public float enemyWalkSpeed;
+    [SerializeField] public float enemyRunSpeed;
+    [SerializeField] private Vector2 centerPoint;
+    private float angle = 0f;
     private Transform player;
-
+    
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        if (centerPoint == Vector2.zero)
+            centerPoint = transform.position;
     }
 
     private void Update()
     {
         if (Vector2.Distance(player.transform.position, transform.position) <= 7)
         {
-            transform.position = Vector2.MoveTowards(transform.position, player.position, enemySpeed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, player.position, enemyRunSpeed * Time.deltaTime);
+        }
+        else
+        {
+            angle -= enemyWalkSpeed * Time.deltaTime;
+            float x = centerPoint.x + Mathf.Cos(angle) * radius;
+            float y = centerPoint.y + Mathf.Sin(angle) * radius;
+            // Применяем новую позицию
+            transform.position = new Vector2(x, y);
         }
     }
 }
