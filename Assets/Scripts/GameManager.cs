05ed2e6c1cb3214dae92293f2e1c616822a2d9b1
Weller26 <Ya.zloy246@gmail.com>
2025-05-03 12:16:48 +0500

    Start making menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,16 +7,11 @@ using UnityEngine.UI;
 public class GameManager: MonoBehaviour
 {
     public Button start, settings, quit, back;
-    public void Restart()
-    {
-        SceneManager.LoadScene("Hub");
-    }
 
     public void StartGame()
     {
         SceneManager.LoadScene("Hub");  
     }
-
     public void Settings()
     {
         start.gameObject.SetActive(false);
@@ -24,12 +19,17 @@ public class GameManager: MonoBehaviour
         quit.gameObject.SetActive(false);
         back.gameObject.SetActive(true);
     }
-
     public void Back()
     {
         start.gameObject.SetActive(true);
         settings.gameObject.SetActive(true);
         quit.gameObject.SetActive(true);
         back.gameObject.SetActive(false);
-    } 
+    }
+
+    public void QuitToMainMenu()
+    {
+        SceneManager.LoadScene("Main menu");
+    }
+    
 }
