ee326026d65e9fb28a2a3b3f7bdf2311ffa0930e
Weller26 <Ya.zloy246@gmail.com>
2025-05-03 11:51:13 +0500

    Add main menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,35 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem.HID;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager: MonoBehaviour
 {
+    public Button start, settings, quit, back;
     public void Restart()
     {
         SceneManager.LoadScene("Hub");
     }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Hub");  
+    }
+
+    public void Settings()
+    {
+        start.gameObject.SetActive(false);
+        settings.gameObject.SetActive(false);
+        quit.gameObject.SetActive(false);
+        back.gameObject.SetActive(true);
+    }
+
+    public void Back()
+    {
+        start.gameObject.SetActive(true);
+        settings.gameObject.SetActive(true);
+        quit.gameObject.SetActive(true);
+        back.gameObject.SetActive(false);
+    } 
 }
