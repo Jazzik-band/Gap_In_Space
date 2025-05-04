6dfaaeb7c23419be1ec68437eecab55c27c46810
Weller26 <Ya.zloy246@gmail.com>
2025-05-04 14:00:47 +0500

    Add menu to hub

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -25,6 +25,7 @@ public class GameManager: MonoBehaviour
             staminaUI.SetActive(false);
             inventoryPanel.SetActive(false);
             batteryValue.SetActive(false);
+            FlashlightController.isFlashLightOn = false;
         }
     }
 
