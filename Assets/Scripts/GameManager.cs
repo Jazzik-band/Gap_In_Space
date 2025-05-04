0d3132981c42805f49c5009b120bc3c0a8d613d9
Weller26 <Ya.zloy246@gmail.com>
2025-05-04 13:47:51 +0500

    Menu buttons update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,19 +8,62 @@ using UnityEngine.UI;
 public class GameManager: MonoBehaviour
 {
     public Button start, settings, quit, back;
-    
+    public GameObject menuPanel, staminaUI, inventoryPanel, batteryValue;
+    public GameObject continueButton, restart, menuSettings, mainMenu, menuQuit, menuBack;
+    // private bool isOpenMenu = false;
+
     public void StartGame()
     {
-        SceneManager.LoadScene("Hub");  
+        SceneManager.LoadScene("Hub");
     }
-    public void Settings()
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            menuPanel.SetActive(true);
+            staminaUI.SetActive(false);
+            inventoryPanel.SetActive(false);
+            batteryValue.SetActive(false);
+        }
+    }
+
+    public void Continue()
+    {
+        menuPanel.SetActive(false);
+        staminaUI.SetActive(true);
+        inventoryPanel.SetActive(true);
+        batteryValue.SetActive(true);
+    }
+
+    public void MainMenuSettings()
     {
         start.gameObject.SetActive(false);
         settings.gameObject.SetActive(false);
         quit.gameObject.SetActive(false);
         back.gameObject.SetActive(true);
     }
-    public void Back()
+
+    public void MenuSettings()
+    {
+        continueButton.SetActive(false);
+        restart.SetActive(false);
+        menuSettings.SetActive(false);
+        mainMenu.SetActive(false);
+        menuQuit.SetActive(false);
+        menuBack.SetActive(true);
+    }
+
+    public void MenuBack()
+    {
+        continueButton.SetActive(true);
+        restart.SetActive(true);
+        menuSettings.SetActive(true);
+        mainMenu.SetActive(true);
+        menuQuit.SetActive(true);
+        menuBack.SetActive(false);
+    }
+    public void MainMenuBack()
     {
         start.gameObject.SetActive(true);
         settings.gameObject.SetActive(true);
@@ -31,6 +74,11 @@ public class GameManager: MonoBehaviour
     public void QuitToMainMenu()
     {
         SceneManager.LoadScene("Main menu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 
 }
