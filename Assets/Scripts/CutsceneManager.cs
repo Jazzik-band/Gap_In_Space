ce2f106bae31e1c2114318241bcf6174c1657875
Weller26 <Ya.zloy246@gmail.com>
2025-05-09 19:12:13 +0500

    Add cutscene

---
+++ Assets/Scripts/CutsceneManager.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class CutsceneManager: MonoBehaviour
+{
+    public static CutsceneManager Instance;
+    [SerializeField] private List<CutsceneStruct> cutscenes = new List<CutsceneStruct>();
+    public static Dictionary<string, GameObject> cutsceneDataBase = new Dictionary<string, GameObject>();
+    public static GameObject activeCutscene;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        foreach (var cutscene in cutsceneDataBase)
+        {
+            cutscene.Value.SetActive(false);
+        }
+    }
+
+    private void InitializeCutsceneDataBase()
+    {
+        cutsceneDataBase.Clear();
+
+        for (int i = 0; i < cutscenes.Count; i++)
+        {
+            cutsceneDataBase.Add(cutscenes[i].cutSceneKey, cutscenes[i].cutSceneObject);
+        }
+    }
+
+    public void StartCutscene(string cutsceneKey)
+    {
+        if (!cutsceneDataBase.ContainsKey(cutsceneKey))
+        {
+            Debug.LogError($"Катсцены с ключом \"{cutsceneKey}\" нету в cutsceneDataBase");
+            return;
+        }
+
+        if (activeCutscene != null)
+        {
+            if (activeCutscene == cutsceneDataBase[cutsceneKey])
+            {
+                return;
+            }
+        }
+
+        activeCutscene = cutsceneDataBase[cutsceneKey];
+
+        foreach (var cutscenes in cutsceneDataBase)
+        {
+            cutscenes.Value.SetActive(false);
+        }
+        
+        cutsceneDataBase[cutsceneKey].SetActive(true);
+    }
+
+    public void EndCutscene()
+    {
+        if (activeCutscene != null)
+        {
+            SceneManager.LoadScene("Main menu");
+            activeCutscene.SetActive(false);
+            activeCutscene = null;
+        }
+    }
+}
+
+[System.Serializable]
+public struct CutsceneStruct
+{
+    public string cutSceneKey;
+    public GameObject cutSceneObject;
+}
